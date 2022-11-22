4d3d2bbb07beeca9a0be86154eb4d21dc95ec5ee
Hester Lai <h.lai52@gmail.com>
2022-11-21 21:42:06 -0800

    Added footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import AddWarehouse from "./components/sections/addWarehouse/addWarehouse";
 import { WarehouseDetailsComponent } from "./components/sections/Warehouse-Details/WarehouseDetailsComponent";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
     <>
       <AddWarehouse />
       <WarehouseDetailsComponent />
+      <Footer />
     </>
   );
 }
