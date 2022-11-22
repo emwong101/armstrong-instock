be01e0928fd16445ef3110e853eaf42839d777c0
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2022-11-21 22:34:42 -0800

    header done, need to fix margin and hover state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Header from "./components/header/Header";
 import AddWarehouse from "./components/sections/addWarehouse/addWarehouse";
 import { WarehouseDetailsComponent } from "./components/sections/Warehouse-Details/WarehouseDetailsComponent";
 
 function App() {
   return (
     <>
+    <Header />
       <AddWarehouse />
       <WarehouseDetailsComponent />
     </>
