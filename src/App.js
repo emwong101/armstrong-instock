5a149ae022c0b49355579a5ab8a26acffc769143
NM <nanayamiki@gmail.com>
2022-11-21 22:33:07 -0800

    added styling for warehouse detail component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 import Footer from "./Components/footer/footer";
 import "./styles/global.scss";
-import AddWarehouse from "./Components/sections/addWarehouse/addWarehouse";
 import { WarehouseDetailsComponent } from "./Components/sections/Warehouse-Details/WarehouseDetailsComponent";
 
 function App() {
 	return (
 		<>
-			<AddWarehouse />
 			<WarehouseDetailsComponent />
 			<Footer />
 		</>
