ca96fd57d929b66ce86a86c08fa77fdf67038f1e
emwong101 <emilywonglfa@gmail.com>
2022-11-22 11:25:21 -0800

    file renaming

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
+import Header from "./components/sections/header/Header";
+import Footer from "./components/sections/footer/Footer";
 import AddWarehouse from "./components/sections/addWarehouse/AddWarehouse";
 import { WarehouseDetailsComponent } from "./components/sections/warehouseDetails/WarehouseDetailsComponent";
 
 function App() {
-	return (
-		<>
-			<Header />
-			<AddWarehouse />
-			<WarehouseDetailsComponent />
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <AddWarehouse />
+      <WarehouseDetailsComponent />
+      <Footer />
+    </>
+  );
 }
 export default App;
