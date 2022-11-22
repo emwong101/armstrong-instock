6930bc94b1aad09e5cdf6fbe5b6ddf307dde779f
NM <nanayamiki@gmail.com>
2022-11-22 09:56:50 -0800

    structured the folders and files. camelCase for folders and Pascal for files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
 import Header from "./components/header/Header";
-import Footer from "./Components/footer/footer";
-import AddWarehouse from "./components/sections/addWarehouse/addWarehouse";
-import { WarehouseDetailsComponent } from "./components/sections/Warehouse-Details/WarehouseDetailsComponent";
+import Footer from "./components/footer/Footer";
+import AddWarehouse from "./components/sections/addWarehouse/AddWarehouse";
+import { WarehouseDetailsComponent } from "./components/sections/warehouseDetails/WarehouseDetailsComponent";
 
 function App() {
-  return (
-    <>
-    <Header />
-      <AddWarehouse />
-      <WarehouseDetailsComponent />
-      <Footer />
-    </>
-  );
-  }
+	return (
+		<>
+			<Header />
+			<AddWarehouse />
+			<WarehouseDetailsComponent />
+			<Footer />
+		</>
+	);
+}
 export default App;
