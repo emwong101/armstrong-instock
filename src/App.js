fe2e5dfedeecaf6ec57303e1153083570e39240a
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2022-11-22 10:24:57 -0800

    files name change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
-import Header from "./components/header/Header";
+import Header from "./Components/header/Header";
 import Footer from "./Components/footer/footer";
-import AddWarehouse from "./components/sections/addWarehouse/addWarehouse";
-import { WarehouseDetailsComponent } from "./components/sections/Warehouse-Details/WarehouseDetailsComponent";
+import AddWarehouse from "./Components/sections/addWarehouse/addWarehouse";
+import { WarehouseDetailsComponent } from "./Components/sections/Warehouse-Details/WarehouseDetailsComponent";
 
 function App() {
   return (
