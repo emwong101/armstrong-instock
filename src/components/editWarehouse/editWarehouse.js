ec7b6fa87674e4da3d0a278abbcff370f0666bf9
emwong101 <emilywonglfa@gmail.com>
2022-11-21 16:55:42 -0800

    created js and scss files for adding warehouse page

---
+++ src/components/editWarehouse/editWarehouse.js
@@ -0,0 +1,11 @@
+import React, { useState, useRef } from "react";
+
+function editWarehouse() {
+  const [warehouseDetails, setWarehouseDetails] = useState({});
+  const [contactDetails, setContactDetails] = useState({});
+
+  const handleSubmit = (key) => {};
+  return <div>editWarehouse</div>;
+}
+
+export default editWarehouse;
