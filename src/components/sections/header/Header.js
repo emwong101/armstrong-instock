35da8f03e71dc8cafc68a709b69a5e78dd141c24
emwong101 <emilywonglfa@gmail.com>
2022-11-22 15:08:02 -0800

    fixed file paths

--- src/components/sections/header/Header.js
+++ src/components/sections/header/Header.js
@@ -4,36 +4,27 @@ import React from "react";
 import Logo from "../../../assets/Logo/InStock-Logo.svg";
 
 const Header = () => {
-	return (
-		<header className="header">
-			<div
-				to={"#"}
-				className="header__company-logo"
-			>
-				<img
-					className="header__logo"
-					src={Logo}
-					alt="Company logo: 2 blue arrows in a square shaped to the left and company name (INSTOCK) in white to the right"
-				></img>
-			</div>
+  return (
+    <header className="header">
+      <div to={"#"} className="header__company-logo">
+        <img
+          className="header__logo"
+          src={Logo}
+          alt="Company logo: 2 blue arrows in a square shaped to the left and company name (INSTOCK) in white to the right"
+        ></img>
+      </div>
 
-			<div className="header__buttons-container">
-				<button
-					className="header__button"
-					type="text"
-				>
-					Warehouses
-				</button>
+      <div className="header__buttons-container">
+        <button className="header__button" type="text">
+          Warehouses
+        </button>
 
-				<button
-					className="header__button"
-					type="text"
-				>
-					Inventory
-				</button>
-			</div>
-		</header>
-	);
+        <button className="header__button" type="text">
+          Inventory
+        </button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/sections/warehouseDetails/WarehouseDetailsComponent.js
+++ src/components/sections/warehouseDetails/WarehouseDetailsComponent.js
@@ -7,96 +7,79 @@ import EditWhite from "../../../assets/Icons/edit_white.svg";
 import "./WarehouseDetailsComponent.scss";
 
 export const WarehouseDetailsComponent = () => {
-	return (
-		<>
-			<div className="wd-container">
-				<section className="title-box">
-					<div className="title-box-left">
-						<img
-							className="title-box_arrow"
-							src={Arrow}
-							alt="arrow"
-						/>
-						<h1 className="title-box_title">Washington</h1>
-					</div>
-					<img
-						src={EditWhite}
-						alt="edit"
-						className="title-box_editing"
-					/>
-				</section>
-				<section className="info-box">
-					<div className="info-box-top">
-						<span className="info-box_label info-box_label-adress">
-							warehouse address:
-						</span>
-						<span className="info-box_detail info-box_address">
-							33 Pearl Street SW, Washington, USA
-						</span>
-					</div>
-					<div className="info-box-bottom">
-						<div className="info-box-bottom_left">
-							<span className="info-box_label info-box_label-name">
-								Contact name:{" "}
-							</span>
-							<span className="info-box_detail info-box_name">Graeme Lyon</span>
-							<span className="info-box_detail info-box_name">
-								Warehouse Manager
-							</span>
-						</div>
-						<div className="info-box-bottom_right">
-							<span className="info-box_label info-box_label-info">
-								Contact information:{" "}
-							</span>
-							<span className="info-box_detail info-box_info">
-								+1 (647) 504-0911
-							</span>
-							<span className="info-box_detail info-box_info">
-								glyon@instock.com
-							</span>
-						</div>
-					</div>
-				</section>
-				<section className="item-box">
-					<div className="item-box-top">
-						<div className="item-box-top_left">
-							<span className="item-box_label item-box_label-item">
-								inventory item
-							</span>
-							<div className="item-box_name-box">
-								<span className="item-box_name">Television</span>
-								<img
-									src={Chevron}
-									alt="closing tag"
-								/>
-							</div>
-							<span className="item-box_label item-box_label-category">
-								category
-							</span>
-							<span className="item-box_category">Electronics</span>
-						</div>
-						<div className="item-box-top_right">
-							<span className="item-box_label item-box_label-status">
-								status
-							</span>
-							<span className="item-box_status">in stock</span>
+  return (
+    <>
+      <div className="wd-container">
+        <section className="title-box">
+          <div className="title-box-left">
+            <img className="title-box_arrow" src={Arrow} alt="arrow" />
+            <h1 className="title-box_title">Washington</h1>
+          </div>
+          <img src={EditWhite} alt="edit" className="title-box_editing" />
+        </section>
+        <section className="info-box">
+          <div className="info-box-top">
+            <span className="info-box_label info-box_label-adress">
+              warehouse address:
+            </span>
+            <span className="info-box_detail info-box_address">
+              33 Pearl Street SW, Washington, USA
+            </span>
+          </div>
+          <div className="info-box-bottom">
+            <div className="info-box-bottom_left">
+              <span className="info-box_label info-box_label-name">
+                Contact name:{" "}
+              </span>
+              <span className="info-box_detail info-box_name">Graeme Lyon</span>
+              <span className="info-box_detail info-box_name">
+                Warehouse Manager
+              </span>
+            </div>
+            <div className="info-box-bottom_right">
+              <span className="info-box_label info-box_label-info">
+                Contact information:{" "}
+              </span>
+              <span className="info-box_detail info-box_info">
+                +1 (647) 504-0911
+              </span>
+              <span className="info-box_detail info-box_info">
+                glyon@instock.com
+              </span>
+            </div>
+          </div>
+        </section>
+        <section className="item-box">
+          <div className="item-box-top">
+            <div className="item-box-top_left">
+              <span className="item-box_label item-box_label-item">
+                inventory item
+              </span>
+              <div className="item-box_name-box">
+                <span className="item-box_name">Television</span>
+                <img src={Chevron} alt="closing tag" />
+              </div>
+              <span className="item-box_label item-box_label-category">
+                category
+              </span>
+              <span className="item-box_category">Electronics</span>
+            </div>
+            <div className="item-box-top_right">
+              <span className="item-box_label item-box_label-status">
+                status
+              </span>
+              <span className="item-box_status">in stock</span>
 
-							<span className="item-box_label item-box_label-qty">QTY</span>
-							<span className="item-box_qty">500</span>
-						</div>
-					</div>
-					<div className="item-box_bottom">
-						<img
-							src={Trash}
-							alt="trash"
-						/>
-						<img
-							src={Edit}
-							alt="trash"
-						/>
-					</div>
-				</section>
-			</div>
-		</>
-	);
+              <span className="item-box_label item-box_label-qty">QTY</span>
+              <span className="item-box_qty">500</span>
+            </div>
+          </div>
+          <div className="item-box_bottom">
+            <img src={Trash} alt="trash" />
+            <img src={Edit} alt="trash" />
+          </div>
+        </section>
+      </div>
+    </>
+  );
 };
