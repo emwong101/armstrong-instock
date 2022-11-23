0323c552f69c3048781e3bea8d681be10c0c0b87
Hester Lai <h.lai52@gmail.com>
2022-11-23 15:27:18 -0800

    Fixed paths spelling

---
+++ src/components/deleteInventoryComponent/DeleteInventoryComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./DeleteInventoryComponent.scss";
+
+const DeleteInventoryComponent = () => {
+  return <div>DeleteInventoryComponent</div>;
+};
+
+export default DeleteInventoryComponent;
--- src/components/deleteInventoryComponent/DeleteInventoryInventoryComponent.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./DeleteInventoryComponent.scss";
-
-const DeleteInventoryInvenoryComponent = () => {
-  return <div>DeleteInventoryInventoryComponent</div>;
-};
-
-export default DeleteInventoryInventoryComponent;
