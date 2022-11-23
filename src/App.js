a3b4fa0f800d7667e55a8b6b15cc4d39d9e12590
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2022-11-23 00:42:39 -0800

    S22BCA-22-Edi-Inventor-Item MOBILE done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/sections/header/Header";
 import Footer from "./components/sections/footer/Footer";
 import AddWarehouse from "./components/sections/addWarehouse/AddWarehouse";
 import { WarehouseDetailsComponent } from "./components/sections/warehouseDetails/WarehouseDetailsComponent";
+import EditInventoryItem from "./components/sections/editInventoryItem/EditInventoryItem";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <AddWarehouse />
       <WarehouseDetailsComponent />
+      <EditInventoryItem  />
       <Footer />
     </>
   );
