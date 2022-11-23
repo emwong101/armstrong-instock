daa3a8a6e0d258d7e3edea07803f2c8a3bc5a5c8
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2022-11-22 17:53:52 -0800

    fixed width and height for header and buttons but still have some work to do on buttons

--- src/components/sections/header/Header.js
+++ src/components/sections/header/Header.js
@@ -5,35 +5,26 @@ import Logo from "../../../assets/Logo/InStock-Logo.svg";
 
 const Header = () => {
 	return (
-		<header className="header">
-			<div
-				to={"#"}
-				className="header__company-logo"
-			>
-				<img
-					className="header__logo"
-					src={Logo}
-					alt="Company logo: 2 blue arrows in a square shaped to the left and company name (INSTOCK) in white to the right"
-				></img>
-			</div>
+    <header className="header">
+      <div to={"#"} className="header__company-logo">
+        <img
+          className="header__logo"
+          src={Logo}
+          alt="Company logo: 2 blue arrows in a square shaped to the left and company name (INSTOCK) in white to the right"
+        ></img>
+      </div>
 
-			<div className="header__buttons-container">
-				<button
-					className="header__button"
-					type="text"
-				>
-					Warehouses
-				</button>
+      <div className="header__buttons-container">
+        <button className="header__button" type="text">
+          <p className="header__button-txt">Warehouses</p>
+        </button>
 
-				<button
-					className="header__button"
-					type="text"
-				>
-					Inventory
-				</button>
-			</div>
-		</header>
-	);
+        <button className="header__button" type="text">
+          <p className="header__button-txt">Inventory</p>
+        </button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
