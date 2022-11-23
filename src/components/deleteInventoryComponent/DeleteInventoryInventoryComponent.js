60bd97b3ab65bcc644f08f2e2dca37b746907492
Hester Lai <h.lai52@gmail.com>
2022-11-23 15:22:11 -0800

    Set up files for component

---
+++ src/components/deleteInventoryComponent/DeleteInventoryInventoryComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./DeleteInventoryComponent.scss";
+
+const DeleteInventoryInvenoryComponent = () => {
+  return <div>DeleteInventoryInventoryComponent</div>;
+};
+
+export default DeleteInventoryInventoryComponent;
