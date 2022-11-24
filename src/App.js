cd95ee328a5703d2ab1b8b1e3b19b57e4b95c8ce
Nanaya Miki <nanayamiki@gmail.com>
2022-11-23 23:07:17 -0800

    fixed some styling name

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import Header from "./components/sections/header/Header";
 import Footer from "./components/sections/footer/Footer";
 import AddWarehouse from "./components/sections/addWarehouse/AddWarehouse";
 import { WarehouseDetailsComponent } from "./components/sections/warehouseDetails/WarehouseDetailsComponent";
-import EditInventoryItem from "./components/sections/editInventoryItem/EditInventoryItem";
 import { WarehouseList } from "./components/sections/warehouseList/WarehouseList";
+import EditInventoryItem from "./components/sections/editInventoryItem/EditInventoryItem";
 
 function App() {
 	return (
 		<div className="main">
 			<Header />
 			<div className="components">
+				<WarehouseList />
 				<WarehouseDetailsComponent />
 				<EditInventoryItem />
 				<AddWarehouse />
