4027927858b5ac32123ffceff93673eafbf21a43
Hester Lai <h.lai52@gmail.com>
2022-11-27 18:17:48 -0800

    closest i can get to mockup without breaking more

--- src/components/atoms/deleteInventoryComponent/DeleteInventoryButton.js
+++ src/components/atoms/deleteInventoryComponent/DeleteInventoryButton.js
@@ -40,30 +40,40 @@ const DeleteInventoryButton = ({ item, onDeleteInventory = () => {} }) => {
         onRequestClose={closeModal}
         contentLabel="Delete Inventory"
       >
-        <div className="delete-i">
-          <div></div>
-          <div></div>
-          <img
-            src={closeButton}
-            className="delete-i__x-button"
-            alt="close button"
-            onClick={closeModal}
-          />
-          <h1 className="delete-i__title">
-            {`Delete ${item_name} inventory item?`}{" "}
-          </h1>
-          <p className="delete-i__details">
-            {`Please confirm that you'd like to delete ${item_name} from the inventory list. You won't be able to undo this action.`}
-          </p>
-          <div className="delete-i__button-container">
-            <button className="delete-i__cancel-btn" onClick={closeModal}>
-              cancel
-            </button>
-            <button className="delete-i__delete-btn" onClick={deleteItem}>
-              delete
-            </button>
+        <div className="delete-">
+          <div className="delete__i-container">
+            <div className="delete__x-container">
+              <img
+                src={closeButton}
+                className="delete-i__x-button"
+                alt="close button"
+                onClick={closeModal}
+              />
+            </div>
+            <h1 className="delete-i__title">
+              {`Delete ${item_name} inventory item?`}{" "}
+            </h1>
+            <p className="delete-i__details">
+              {`Please confirm that you'd like to delete ${item_name} from the inventory list. You won't be able to undo this action.`}
+            </p>
+
+            <div className="outsidediv">
+              <div className="delete-i__button-container">
+                <div className="extradiv">
+                  <button className="delete-i__cancel-btn" onClick={closeModal}>
+                    cancel
+                  </button>
+                </div>
+                {/* //cancelout that div for issues below */}
+                <div className="delete-i__container">
+                  <button className="delete-i__delete-btn" onClick={deleteItem}>
+                    delete
+                  </button>
+                </div>
+              </div>
+               
+            </div>
           </div>
-           
         </div>
       </Modal>
     </>
